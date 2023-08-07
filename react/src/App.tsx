930da5bda90fe53993ff21823e36bd67a5d7e617
wolfman0719 <hsato0719@icloud.com>
2023-08-07 15:19:12 +0900

    Add files via upload

---
+++ react/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './components/Adbk.tsx';
+
+function App() {
+  return (
+    <div className="App">
+    <Adbk />
+    </div>
+  );
+}
+
+export default App;
